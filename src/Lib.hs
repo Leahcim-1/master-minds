82c28d24a4e40d5feb11fb6467aacd86abbc6e07
Dx724 <dx72dx@gmail.com>
2021-12-03 13:04:45 -0500

    Implement guessResult

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,9 +2,15 @@ module Lib
     ( 
     ) where
 
+import Data.List
+
 
 
 data Peg = White | Black
+
+instance Show Peg where
+    show Black = "Black"
+    show White = "White"
 
 type ResponsePegs = [Peg]
 
@@ -16,7 +22,11 @@ generateCodeSet :: Ord a => [a] -> CodeSet a
 generateCodeSet list = error ""
 
 guessResult :: Ord a => Code a -> Code a -> ResponsePegs
-guessResult ans guess = error ""
+guessResult ans guess = replicate numBlack Black ++ replicate numWhite White
+    where numBlack = length $ filter id $ zipWith (==) ans guess
+          numWhite = sum (map minCodeCount $ nub guess) - numBlack
+          minCodeCount v = min (count v ans) (count v guess)
+          count v ls = length $ filter (==v) ls
 
 generateNextGuess :: Ord a => CodeSet a -> Code a
 generateNextGuess codeset = error ""
