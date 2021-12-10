3daad6867a30fcc125fd4f1f7a9d2337f95d1bb9
leahcimsu <leahcimsu@tencent.com>
2021-12-10 13:18:53 -0500

    fix: readLn type

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,21 @@ module Main where
 
 import Lib
 
+readInt :: IO Int
+readInt = readLn
+
 main :: IO ()
 main = do
     putStrLn "Game Configuration: How many colors?"
-    numColors <- readLn -- Should do error handling
+    numColors <- readInt-- Should do error handling
     putStrLn "Game Configuration: How many holes?"
-    numHoles <- readLn
+    numHoles <- readInt
     putStrLn $ "Config: " ++ (show (numColors::Int)) ++ " colors and "
         ++ (show (numHoles::Int)) ++ " holes"
     putStrLn "What is the solution code?"
-    solStr <- readLn
+    solStr <- readInt
     -- TODO: verify solStr length and valid symbols, convert to Code
     let sol = [1, 2, 3, 3] :: Code Int -- TODO: remove
     let codeset = generateCodeSet [1..numColors] numHoles
-    putStr codeset
+    putStr "hi"
     -- TODO: step through and solve
