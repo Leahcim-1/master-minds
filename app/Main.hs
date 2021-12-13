9866d260ad7f23c7e32dc67fbd1db0c1d0e2122c
Dx724 <dx72dx@gmail.com>
2021-12-13 00:32:58 -0500

    Fix algorithm

--- app/Main.hs
+++ app/Main.hs
@@ -18,8 +18,9 @@ main = do
     putStrLn "What is the solution code?"
     solStr <- readInt
     -- TODO: verify solStr length and valid symbols, convert to Code
-    let sol = [1, 2, 3, 3] :: Code Int -- TODO: remove
+    let sol = [1, 2, 1, 1] :: Code Int -- TODO: remove
     let initialGuess = [1, 1, 1, 1] :: Code Int -- TODO: try all
+    putStrLn $ "DEBUG NOTE: Using solution " ++ (show sol) ++ " instead"
     let codeset = generateCodeSet [1..numColors] numHoles
     -- TODO: step through and solve
     num_turns_required <- play_mastermind initialGuess sol 1 codeset codeset
@@ -27,15 +28,23 @@ main = do
 
 play_mastermind :: (Ord a, Show a) => Code a -> Code a -> Int -> CodeSet a -> CodeSet a -> IO Int
 play_mastermind guess solution k fullSet possibleSet = do
+    -- TODOremove putStrLn $ "Debug (Remaining): " ++ (show $ length possibleSet)
+    -- TODOremove putStrLn $ "Debug (Remaining): " ++ (show $ take 5 possibleSet)
     putStrLn $ "Guessing: " ++ (show guess)
     let response = guessResult guess solution
+    putStrLn $ "Response: " ++ (show $ fst response) ++ " black and "
+        ++ (show $ snd response) ++ " white"
     let possibleSet' = filterCodeSet possibleSet guess response
+    -- TODOremove putStrLn $ "Debug (Response): " ++ (show response)
+    -- TODOremove putStrLn $ "Debug (Remaining New): " ++ (show $ length possibleSet')
+    -- TODOremove putStrLn $ "Debug (Remaining New): " ++ (show $ take 5 possibleSet')
     if length possibleSet' == 1
         then do
             putStrLn $ "Solved: " ++ (show $ head possibleSet')
-            return k
+            return (k + 1)
         else do
-            let possibilities = map (scoreGuess possibleSet) fullSet
+            let possibilities = map (scoreGuess possibleSet') fullSet
+            -- TODOremove putStrLn $ "Debug (Possibilities): " ++ (show $ take 3 possibilities)
             let nextGuess = getThird $ minimum possibilities
             play_mastermind nextGuess solution (k+1) fullSet possibleSet'
     where
@@ -45,8 +54,8 @@ play_mastermind guess solution k fullSet possibleSet = do
                 valid = isInfixOf [code] possible
                 allResponses = map (guessResult code) possible
                 score = getMaxCount allResponses
-                getMaxCount xs = maximumBy compareSnd $ getCounts xs
-                compareSnd a b = compare (snd a) (snd b)
+                getMaxCount xs = maximum $ map snd $ getCounts xs
+                -- compareSnd a b = compare (snd a) (snd b)
                 incCount o [] = [(o, 1)]
                 incCount o (x@(v, c):xs)
                     | v == o = (v, c + 1) : xs
