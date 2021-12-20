367cefeb72c01897553b6aa7603214686a7e399b
leahcimsu <leahcimsu@tencent.com>
2021-12-20 16:53:12 -0500

    feat: format the code

--- app/Main.hs
+++ app/Main.hs
@@ -1,62 +1,38 @@
 module Main where
 
-import Lib
-import Data.List(isInfixOf, maximumBy)
+import Data.Function (on)
+import Data.List (isInfixOf, minimumBy)
+import Debug.Trace (trace)
+import Lib (generateCodeSet, intToCode, playMastermind)
+
+debug :: Show a => a -> a
+debug x = trace ("Debug: " ++ show x) x
 
 {- Bind Int to readLn -}
 readInt :: IO Int
 readInt = readLn
 
+{- Bind String to readLn -}
+readStr :: IO String
+readStr = readLn
+
 main :: IO ()
 main = do
     putStrLn "Game Configuration: How many colors?"
-    numColors <- readInt-- Should do error handling
+    numColors <- readInt -- Should do error handling
     putStrLn "Game Configuration: How many holes?"
     numHoles <- readInt
-    putStrLn $ "Config: " ++ (show (numColors::Int)) ++ " colors and "
-        ++ (show (numHoles::Int)) ++ " holes"
+    putStrLn $
+        "Config: " ++ show (numColors :: Int) ++ " colors and "
+        ++ show (numHoles :: Int)
+        ++ " holes"
     putStrLn "What is the solution code?"
     solStr <- readInt
     -- TODO: verify solStr length and valid symbols, convert to Code
-    let sol = [1, 2, 1, 1] :: Code Int -- TODO: remove
-    let initialGuess = [1, 1, 1, 1] :: Code Int -- TODO: try all
-    putStrLn $ "DEBUG NOTE: Using solution " ++ (show sol) ++ " instead"
-    let codeset = generateCodeSet [1..numColors] numHoles
-    num_turns_required <- play_mastermind initialGuess sol 1 codeset codeset
-    putStrLn $ "Solved in " ++ (show num_turns_required) ++ " turns"
-
-play_mastermind :: (Ord a, Show a) => Code a -> Code a -> Int -> CodeSet a -> CodeSet a -> IO Int
-play_mastermind guess solution k fullSet possibleSet = do
-    -- TODOremove putStrLn $ "Debug (Remaining): " ++ (show $ length possibleSet)
-    -- TODOremove putStrLn $ "Debug (Remaining): " ++ (show $ take 5 possibleSet)
-    putStrLn $ "Guessing: " ++ (show guess)
-    let response = guessResult guess solution
-    putStrLn $ "Response: " ++ (show $ fst response) ++ " black and "
-        ++ (show $ snd response) ++ " white"
-    let possibleSet' = filterCodeSet possibleSet guess response
-    -- TODOremove putStrLn $ "Debug (Response): " ++ (show response)
-    -- TODOremove putStrLn $ "Debug (Remaining New): " ++ (show $ length possibleSet')
-    -- TODOremove putStrLn $ "Debug (Remaining New): " ++ (show $ take 5 possibleSet')
-    if length possibleSet' == 1
-        then do
-            putStrLn $ "Solved: " ++ (show $ head possibleSet')
-            return (k + 1)
-        else do
-            let possibilities = map (scoreGuess possibleSet') fullSet
-            -- TODOremove putStrLn $ "Debug (Possibilities): " ++ (show $ take 3 possibilities)
-            let nextGuess = getThird $ minimum possibilities
-            play_mastermind nextGuess solution (k+1) fullSet possibleSet'
-    where
-        getThird (_,_,x) = x
-        scoreGuess possible code = (score, valid, code)
-            where
-                valid = isInfixOf [code] possible
-                allResponses = map (guessResult code) possible
-                score = getMaxCount allResponses
-                getMaxCount xs = maximum $ map snd $ getCounts xs
-                -- compareSnd a b = compare (snd a) (snd b)
-                incCount o [] = [(o, 1)]
-                incCount o (x@(v, c):xs)
-                    | v == o = (v, c + 1) : xs
-                    | otherwise = x : incCount o xs
-                getCounts xs = foldr incCount [] xs
+    -- let sol = [1, 2, 1, 1]
+    let sol = intToCode solStr
+    let initialGuess = replicate numHoles 1 
+    -- putStrLn $ "DEBUG NOTE: Using solution " ++ show sol ++ " instead"
+    let codeset = generateCodeSet [1 .. numColors] numHoles
+    num_turns_required <- playMastermind initialGuess sol 1 codeset codeset
+    putStrLn $ "Solved in " ++ show num_turns_required ++ " turns"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,31 +1,49 @@
 module Lib
     (
-        Code,
-        CodeSet,
+        intToCode,
         generateCodeSet,
         guessResult,
         filterCodeSet,
-        generateNextGuess
+        generateNextGuess,
+        compareCode,
+        minimumPossibility,
+        scoreGuess,
+        playMastermind
     ) where
-import Data.List(nub)
+import Data.List (isInfixOf, minimumBy, nub)
+import Data.Function (on)
+
+import Debug.Trace (trace)
+
+
+debug :: Show a => a -> a
+debug x = trace ("Debug: " ++ show x) x
 
 
 type ResponsePegs = (Int, Int) -- (#black, #white)
 
-type Code a = [a]
+type Code = [Int]
 
-type CodeSet a = [Code a] -- TODO: Use set instead of list for speed?
+type CodeSet = [Code] -- TODO: Use set instead of list for speed?
 
-type Possibility a = (Int, Bool, Code a) -- (Score, Invalid, Code)
+type Possibility = (Int, Bool, Code) -- (Score, Invalid, Code)
+
+
+intToCode :: Int -> Code
+intToCode x = reverse $ intToCode' x
+    where intToCode' x'   
+            | x' == 0    = []
+            | otherwise = r : intToCode' q
+            where (q, r) = x' `divMod` 10
 
 generateCodeSet :: (Eq a, Num a) => [a] -> a -> [[a]]
 generateCodeSet [] _ = error "Give me a non-empty list"
-generateCodeSet list hole 
+generateCodeSet list hole
     | hole == 1           = [ [x] | x <- list]
     | otherwise           = [ x:xs | x <- list, xs <- generateCodeSet list $ hole - 1]
-    
 
-guessResult :: Ord a => Code a -> Code a -> ResponsePegs
+
+guessResult :: Code -> Code-> ResponsePegs
 guessResult ans guess = (numBlack, numWhite)
     where numBlack = length $ filter id $ zipWith (==) ans guess
           numWhite = sum (map minCodeCount $ nub guess) - numBlack
@@ -39,9 +57,79 @@ guessResult ans guess = (numBlack, numWhite)
     evaluate guesses given some possible response; use numBlack and first part of numWhite above to
     do this filtering)
 -}
-filterCodeSet :: Ord a => CodeSet a -> Code a -> ResponsePegs -> CodeSet a
+filterCodeSet :: CodeSet -> Code -> ResponsePegs -> CodeSet
 filterCodeSet set guess response =
     filter ((response ==) . guessResult guess) set
 
-generateNextGuess :: Ord a => CodeSet a -> ResponsePegs -> Code a
+generateNextGuess :: CodeSet -> ResponsePegs -> Code
 generateNextGuess codeset resp = error ""
+
+
+compareCode :: Code -> Code -> Ordering
+compareCode [] [] = EQ
+compareCode [x] [y]
+    | x < y     = LT
+    | x > y     = GT
+    | x == y    = EQ
+compareCode codeA@(x: xs) codeB@(y: ys)
+    | cmp == EQ = compareCode xs ys
+    | otherwise = cmp
+    where cmp = compareCode [x] [y]
+compareCode _ _ = error "Unmatch length of code"
+
+
+minimumPossibility :: [Possibility ] -> Possibility
+minimumPossibility p
+    | length minLists == 1      = head minLists
+    | length validMinLists == 1 = head validMinLists
+    | null validMinLists        = smallTieBreaker p
+    | otherwise                 = smallTieBreaker validMinLists
+    where
+        getThird (_, _, x)      = x
+        (minScore, _, _)        = minimum p
+        minLists                = filter (\(score, _, _) -> minScore == score) p
+        validMinLists           = validTieBreaker minLists
+        validTieBreaker list    = filter (\(_, v, _) -> v) $ debug list
+        smallTieBreaker list    = minimumBy (\(_, _, codeA) (_, _, codeB) -> compareCode codeA codeB) list
+
+
+scoreGuess :: [Code] -> Code -> Possibility
+scoreGuess possible code = (score, valid, code)
+    where
+        valid = code `elem` possible
+        allResponses = map (guessResult code) possible
+        score = getMaxCount allResponses
+        getMaxCount xs = maximum $ map snd $ getCounts xs
+        -- compareSnd a b = compare (snd a) (snd b)
+        incCount o [] = [(o, 1)]
+        incCount o (x@(v, c) : xs)
+            | v == o = (v, c + 1) : xs
+            | otherwise = x : incCount o xs
+        getCounts xs = foldr incCount [] xs
+
+
+playMastermind ::  Code -> Code -> Int -> CodeSet -> CodeSet -> IO Int
+playMastermind guess solution k fullSet possibleSet = do
+  -- TODOremove putStrLn $ "Debug (Remaining): " ++ (show $ length possibleSet)
+  -- TODOremove putStrLn $ "Debug (Remaining): " ++ (show $ take 5 possibleSet)
+  putStrLn $ "Guessing: " ++ show guess
+  let response = guessResult guess solution
+  putStrLn $
+    "Response: " ++ show (fst response) ++ " black and "
+      ++ show (snd response)
+      ++ " white"
+  let possibleSet' = filterCodeSet possibleSet guess response
+  -- TODOremove putStrLn $ "Debug (Response): " ++ (show response)
+  -- TODOremove putStrLn $ "Debug (Remaining New): " ++ (show $ length possibleSet')
+  -- TODOremove putStrLn $ "Debug (Remaining New): " ++ (show $ take 5 possibleSet')
+  if length possibleSet' == 1
+    then do
+      putStrLn $ "Solved: " ++ show (head possibleSet')
+      return (k + 1)
+    else do
+      let possibilities = map (scoreGuess possibleSet') fullSet
+      -- TODOremove putStrLn $ "Debug (Possibilities): " ++ (show $ take 3 possibilities)
+      let nextGuess = getThird $ minimum possibilities
+      playMastermind nextGuess solution (k + 1) fullSet possibleSet'
+  where
+    getThird (_, _, x) = x
